f7016aea189d18f4dbeb109f498590c2eeb2bf6c
Tom Willemsen <tom@ryuslash.org>
2012-07-31 02:48:00 +0200

    Add GPL comments

--- src/gitto-path.c
+++ src/gitto-path.c
@@ -1,3 +1,22 @@
+/* gitto -- Keep track of your git repositories
+   Copyright (C) 2012 Tom Willemsen <tom at ryuslash dot org>
+
+   This file is part of gitto.
+
+   gitto is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   gitto is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with gitto.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdlib.h>
 #include <libguile.h>
 
